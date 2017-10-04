be850bd854b0c91dcc599a27b0fa8ac466218b42
Sabin <spradh@users.noreply.github.com>
2017-10-03 23:19:44 -0400

    Update RScript.r

--- RScript.r
+++ RScript.r
@@ -3,6 +3,8 @@ names(factorData)
 
 View(factordata)
 
+
+#Calculating Log Returns
 ln_returns<-vector()
 
 for(i in 1:length(factorData$ticker)){
@@ -15,19 +17,17 @@ for(i in 1:length(factorData$ticker)){
 
 ln_returns
 
+#Combining Log Returns to factor data 
 factorData<-cbind(ln_returns,factorData)
 
-
+#Taking out outliers
 factorDataReduced<-subset(factorData,ln_returns>-1000)
 
 View(factorDataReduced)
 
 plot(factorDataReduced$ln_returns,factorDataReduced$eps)
 
-
+#Selecting Specified date
 factordata_reduced<-subset(factordata,calendardate==20110630)
-
-
-
 
 plot(factordata_reduced$ln_returns,factordata_reduced$eps)
