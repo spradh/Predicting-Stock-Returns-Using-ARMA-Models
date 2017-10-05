9c75839ff4ba05f37dcaf30640faa1a5fc53b876
Sabin <spradh@users.noreply.github.com>
2017-10-05 01:16:52 -0400

    Update RScript.r

--- RScript.r
+++ RScript.r
@@ -1,7 +1,7 @@
 
 factorData<-read.csv('dataFileARQmod.csv')
-names(factorData)
-
+names<-names(factorData)
+sort(names)
 View(factorData)
 
 
@@ -28,16 +28,80 @@ View(factorDataReduced)
 
 #Selecting Specified date
 factorDataReducedDate<-subset(factorDataReduced,calendardate==20110630)
-
+View(factorDataReducedDate)
 plot(factorDataReducedDate$eps, factorDataReducedDate$ln_returns)
 
 
 #selected variables
 
-var=c('PE1', 'PB', 'ROIC','ROA','ASSETTURNOVER','EBITDA','MARKETCAP','DE','BVPS','CURRENTRATIO','FCFPS','ROIC','ASSETSC','PPNENETS','LIABILITIES','EQUITY','EBIT','REVENUE','RND','CONSOLINC')
+(factorDataReducedDate[,var[1]]-mean(factorDataReducedDate[,var[1]]))/sd(factorDataReducedDate[,var[1]])
 
-models=c()
+mean(factorDataReducedDate[,var[1]])
 
-for(i in 1:length(var)){
-  model[i]=lm(ln_returns~var[i], data=factorDataReduced)
-}
+#PE1
+plot(factorDataReducedDate$pe1, factorDataReducedDate$ln_returns)
+model_pe1=lm(ln_returns~pe1, data=factorDataReducedDate)
+summary(model_pe1)
+
+#marketcap *
+plot(factorDataReducedDate$marketcap, factorDataReducedDate$ln_returns)
+model_marketcap=lm(ln_returns~marketcap, data=factorDataReducedDate)
+summary(model_marketcap)
+
+#pb
+model_pb=lm(ln_returns~pb, data=factorDataReducedDate)
+summary(model_pb)
+
+#pb
+model_assetturnover=lm(ln_returns~assetturnover, data=factorDataReducedDate)
+summary(model_assetturnover)
+
+#ebitda *
+plot(factorDataReducedDate$ebitda, factorDataReducedDate$ln_returns)
+model_ebitda=lm(ln_returns~ebitda, data=factorDataReducedDate)
+summary(model_ebitda)
+
+#de
+model_de=lm(ln_returns~de, data=factorDataReducedDate)
+summary(model_de)
+
+#bvps **
+model_bvps=lm(ln_returns~bvps, data=factorDataReducedDate)
+summary(model_bvps)
+plot(factorDataReducedDate$bvps, factorDataReducedDate$ln_returns)
+
+
+#currentratio
+model_currentratio=lm(ln_returns~currentratio, data=factorDataReducedDate)
+summary(model_currentratio)
+
+#fcfps *
+model_fcfps=lm(ln_returns~fcfps, data=factorDataReducedDate)
+summary(model_fcfps)
+
+#assetsc *
+model_assetsc=lm(ln_returns~assetsc, data=factorDataReducedDate)
+summary(model_assetsc)
+
+#liabilities
+model_liabilities=lm(ln_returns~liabilities, data=factorDataReducedDate)
+summary(model_liabilities)
+
+#equity
+model_equity=lm(ln_returns~equity, data=factorDataReducedDate)
+summary(model_equity)
+
+#ebit *
+model_ebit=lm(ln_returns~ebit, data=factorDataReducedDate)
+summary(model_ebit)
+
+#revenue
+model_revenue=lm(ln_returns~revenue, data=factorDataReducedDate)
+summary(model_revenue)
+
+#rnd
+model_rnd=lm(ln_returns~rnd, data=factorDataReducedDate)
+summary(model_rnd)
+
+
+
