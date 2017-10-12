a7f5bc8947c9d10422ce98f50e310a656ff94504
Sabin <spradh@users.noreply.github.com>
2017-10-12 13:08:15 -0400

    Update rscript3.r

--- Project1/rscript3.r
+++ Project1/rscript3.r
@@ -1,2 +1,18 @@
 #importing final model
 data=read.csv('finalData.csv')
+
+dates<-unique(data$calendardate)
+
+model_list<-vector()
+
+for(i in 1:15){
+  a<-paste("modelDate_",dates[i],sep="")
+  model_list<-c(model_list, a)
+  model<-lm(ln_returns~.,data=data)
+  assign(a, model)
+}
+
+for(i in model_list){
+
+}
+  
